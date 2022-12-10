c924d2c8dfa7704bb70109436b92376af949c666
eunmin1015 <jangeunmin38@gmail.com>
2022-12-10 18:13:41 +0900

    main.c commit

--- main.c
+++ main.c
@@ -1,41 +1,44 @@
+#include <getopt.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <sys/ipc.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h> // for open/close
+#include <fcntl.h> // for O_RDWR//
+#include <sys/ioctl.h> // for ioctl
+#include <sys/msg.h>
+#include <pthread.h>
+#include <linux/fb.h>
+#include <time.h>
+
+
+#include "led.h"
+#include "fnd.h"
+#include "acc.h"
+#include "button.h"
+#include "buzzer.h"
+#include "textlcd.h"
+#include "colorled.h"
+#include "Temperature.h"
+#include "bitmap.h"
+#include "embe.h"
 #include "touch.h"
 
-int main(void)
-{
-    touchInit();
-    int msgID = msgget (MESSAGE_ID, IPC_CREAT | 0666);
-    TOUCH_MSG_T B;
+BUTTON_MSG_T B;
+TOUCH_MSG_T T;
+int msgID;
+int fd;
+
+int main(int argc, char* argv[]) {
+   
+	setInit(); //각종 기기들을 키고  초기 설정을 해준다.
+	bitmainfunc("gamestart.bmp"); //메인 메뉴 사진을 띄운다.
     while(1);
     {
-        msgrcv(msgID, &B, sizeof(B)- sizeof(long int), 0, 0);
-
-        switch (B.keyInput)
-        {
-            case 999:
-                if(B.pressed == 1)
-                {
-                    if(B.x < 100 && B.y <100)
-                    {
-                        printf("left top");
-                    }
-                    else if (B.x > 500 && B.y < 100)
-                    {
-                        printf("right top");
-                    }
-                    else if (B.x <100 && B.y > 300)
-                    {
-                        printf("left bottom");
-                    }
-                    else if (B.x > 500 && B.y > 300)
-                    {
-                        printf("right bottom");
-                    }
-                    else printf("hmm... %d, %d", B.x, B.y);
-                }
-        }
-break;
-    }
-
+		
+	}
+return 0;
 }
